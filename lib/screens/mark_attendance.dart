cf13d9088f4fd17075cd3a4a5c182cc4b7c39d19
Olusegun Adesina <shegmatech@gmail.com>
2022-09-22 20:59:32 +0100

    onTap implemented , TODO : getLocation

--- lib/screens/mark_attendance.dart
+++ lib/screens/mark_attendance.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class markAttendance extends StatefulWidget {
   const markAttendance({Key? key}) : super(key: key);
@@ -8,25 +10,49 @@ class markAttendance extends StatefulWidget {
 }
 
 class _markAttendanceState extends State<markAttendance> {
+  bool _isAttendance = true;
 
-  bool isAttendance = true;
+  void getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
 
+//handle tap function
+  void _handleTap() {
+    setState(() {
+      _isAttendance = !_isAttendance;
+      print(_isAttendance);
+    });
+  }
+
+//handle tap end
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
-          onTap: (){
-            //TODO: show dialog box for attendance marked and change the signin buttin to signout with redBG
-            setState(() {
-              isAttendance = false;
-            });
-            print(isAttendance);
-          },
-          child: Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(shape: BoxShape.circle, color: isAttendance? Colors.green : Colors.red)
-          ))
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: GestureDetector(
+                  onTap: _handleTap,
+                  child: Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _isAttendance
+                              ? Colors.green
+                              : Colors
+                                  .red) //color: isAttendance? Colors.green : Colors.red
+                      ))),
+          SizedBox(height: 20.0,),
+          GestureDetector(
+            onTap: getLocation,
+            child: Text("Location here"),
+          )
+        ],
+      ),
     );
   }
 }
