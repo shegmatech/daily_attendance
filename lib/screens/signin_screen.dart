8fdc4d43648d26ac496bd44459f755407c0b2991
Olusegun Adesina <shegmatech@gmail.com>
2022-10-18 08:43:53 +0100

    signin implemented with reqres api TODO: after signin save user token to local storage and navigate to mark attendance page . prevent future signin unles the user signs out

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,15 +1,49 @@
+import 'dart:convert';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:daily_attendance/Constants/reusables.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 import 'mark_attendance.dart';
 
 
 
-class SigninScreen extends StatelessWidget {
+class SigninScreen extends StatefulWidget {
 
+  @override
+  State<SigninScreen> createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+
+  //Login Function
+  Future<void> login(String email, password) async{
+    try{
+      Response response = await post(
+          Uri.parse('https://reqres.in/api/login'),
+          body:{
+                'email': email,
+                'password': password,
+      });
+
+      if(response.statusCode == 200){
+        var data = jsonDecode(response.body.toString());
+        print(data['token']);
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder:(_) => markAttendance()));
+
+      }
+
+    }catch(e){
+      print(e.toString());
+      print('Invalid Credential');
+    }
+  }
+
 
 
 
@@ -53,9 +87,9 @@ class SigninScreen extends StatelessWidget {
                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
                           color: Colors.red.shade800,
                           onPressed: () {
-                            Navigator.of(context).pushReplacement(MaterialPageRoute(builder:(_) => markAttendance()));
-                            //Provider.of<Auth>(context, listen: false).login(emailController.text, passwordController.text);
-                            //Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => AttendanceScreen()));
+                            login(emailController.text.toString(), passwordController.text.toString());
+                            // Navigator.of(context).pushReplacement(MaterialPageRoute(builder:(_) => markAttendance()));
+
                           },
                           child: const Text('Login', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white,),),
                         ),
@@ -71,3 +105,4 @@ class SigninScreen extends StatelessWidget {
         );
   }
 }
+
